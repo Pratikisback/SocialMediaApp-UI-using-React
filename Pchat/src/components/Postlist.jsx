e732a08c010531fd07fd88359fc4efd36c69efe3
Pratik-Chavan-01 <pratik.c@codearray.tech>
2023-12-18 17:11:29 +0530

    Finished and started with React Routers

--- Pchat/src/components/Postlist.jsx
+++ Pchat/src/components/Postlist.jsx
@@ -3,38 +3,10 @@ import Post from "./Post";
 import { PostList as PostListData } from "../store/PostListProvider";
 import WelcomeMessage from "./WelcomeMessage";
 import LoadingSpinner from "./LoadingSpinner";
+import { v4 as uuidv4 } from "uuid";
 
 const Postlist = () => {
-  const { postList, addInitialPosts } = useContext(PostListData);
-
-  const [fetching, setFetching] = useState(false);
-
-  useEffect(() => {
-    setFetching(true);
-    const controller = new AbortController();
-    const signal = controller.signal;
-    console.log("fetching started");
-    let data = { get_posts: true };
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    };
-    fetch("http://127.0.0.1:5000/user/get-posts", options, { signal })
-      .then((res) => res.json())
-      .then((dataa) => {
-        addInitialPosts(dataa.posts);
-        console.log("fetch returned");
-        setFetching(false);
-      });
-
-    return () => {
-      console.log("Cleaning up useEffect");
-      controller.abort();
-    };
-  }, []);
+  const { postList, fetching } = useContext(PostListData);
 
   const handleGetPosts = () => {};
   return (
@@ -44,7 +16,7 @@ const Postlist = () => {
         <WelcomeMessage onGetPosts={handleGetPosts} />
       )}
       {!fetching &&
-        postList.map((post) => <Post key={post.id} post={post}></Post>)}
+        postList.map((post) => <Post key={post.title} post={post}></Post>)}
     </>
   );
 };
